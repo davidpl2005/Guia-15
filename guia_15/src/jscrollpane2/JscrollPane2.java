207edd9a83c2870a70b7c2f62ea6013c5a44b068
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-24 16:58:44 -0500

    Jscrollpane

---
+++ guia_15/src/jscrollpane2/JscrollPane2.java
@@ -0,0 +1,20 @@
+
+package jscrollpane2;
+
+/**
+ *
+ * @author isac pacheco
+ */
+public class JscrollPane2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+    }
+    
+}
